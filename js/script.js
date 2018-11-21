9af06704de72af5559b2dae8d58ba10e82cb59a2
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-21 17:53:08 +0000

    automatically backed up by learn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // code to be executed goes here
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script> ;
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script> 
   <script>
    $("#text").append("this is so freaking cool.");
   </script>
